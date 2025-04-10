a6569fac4a07a9be3cf1c616807254ee2969ceca
Anand Panda <anand612p@gmail.com>
2025-04-10 16:49:21 +0530

    Add CourseList component and integrate GraphQL for course data retrieval

--- skillX/app/(tabs)/home.tsx
+++ skillX/app/(tabs)/home.tsx
@@ -1,22 +1,28 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Header from "../Components/HomeScreen/Header";
+import CourseList from "../Components/HomeScreen/CourseList";
 
 const home = () => {
-  return (
-    <View style={styles.headerSection}>
-      <Header />
-    </View>
-  );
+    return (
+        <>
+            <View style={styles.headerSection}>
+                <Header />
+            </View>
+            <View>
+                <CourseList level={"basic"} />
+            </View>
+        </>
+    );
 };
 
 export default home;
 
 const styles = StyleSheet.create({
-  headerSection: {
-    backgroundColor: "#6857E8",
-    width: "100%",
-    height: 250,
-    padding: 20,
-  },
+    headerSection: {
+        backgroundColor: "#6857E8",
+        width: "100%",
+        height: 250,
+        padding: 20,
+    },
 });
