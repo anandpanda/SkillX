e1812a57cb93a6b7b1ed6414036c463ab8d96c4c
rahul <rahulbky2004@gmail.com>
2025-04-28 22:49:47 +0530

    Update API base URL to point to the correct server address

--- client/app/Services/api.ts
+++ client/app/Services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.1.5:8080/api",
+    baseURL: "http://192.168.1.4:8080/api",
+    // baseURL: "http://192.168.1.5:8080/api",
     timeout: 10000,
 });
 
